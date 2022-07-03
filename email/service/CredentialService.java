b51d5f41db8e576f775e91bf7805caa24495bab8
Vamsi-GL <105546360+Vamsi-GL@users.noreply.github.com>
2022-07-03 14:43:34 +0530

    Removing lab submission from Assignment repository

--- email/service/CredentialService.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.email.service;
-
-import com.greatlearning.email.model.Employee;
-
-public interface CredentialService {
-    String generateEMailAddress(Employee emp,String department);
-    char[] generatePassword();
-    void showCredential(Employee emp,String email,char[] generatedPassword);
-}
